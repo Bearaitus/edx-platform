48ca9906ce3fcdadc33bb154d1cc27c9d4fedd92
Ivan Petrov <ebkrc5bsfekd@mail.ru>
2025-07-16 15:25:38 +0300

    Страница

--- xmodule/unit_block.py
+++ xmodule/unit_block.py
@@ -38,7 +38,7 @@ class UnitBlock(XBlock):
         display_name=_("Display Name"),
         help=_("The display name for this component."),
         scope=Scope.settings,
-        default=_("Unit"),
+        default=_("Страница"),
     )
 
     def student_view(self, context=None):
