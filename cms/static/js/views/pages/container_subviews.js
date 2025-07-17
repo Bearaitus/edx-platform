72c9572e08fa8abaf5b986aedacd0c8ff27347d9
Ivan Petrov <ebkrc5bsfekd@mail.ru>
2025-07-17 13:51:53 +0300

    Publishing и adding

--- cms/static/js/views/pages/container_subviews.js
+++ cms/static/js/views/pages/container_subviews.js
@@ -166,7 +166,7 @@ function($, _, gettext, BaseView, ViewUtils, XBlockViewUtils, MoveXBlockUtils, H
             if (e && e.preventDefault) {
                 e.preventDefault();
             }
-            ViewUtils.runOperationShowingMessage(gettext('Publishing'),
+            ViewUtils.runOperationShowingMessage(gettext('Публикация'),
                 function() {
                     return xblockInfo.save({publish: 'make_public'}, {patch: true});
                 }).always(function() {
