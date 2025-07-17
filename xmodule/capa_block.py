4274f0a79d3cb59514c42132c4656132ead76f65
Ivan Petrov <ebkrc5bsfekd@mail.ru>
2025-07-17 16:29:15 +0300

    Blank Problem -> Вопрос, удаление дублирования

--- xmodule/capa_block.py
+++ xmodule/capa_block.py
@@ -179,7 +179,7 @@ class ProblemBlock(
         scope=Scope.settings,
         # it'd be nice to have a useful default but it screws up other things; so,
         # use display_name_with_default for those
-        default=_("Blank Problem")
+        default=_("Вопрос")
     )
     attempts = Integer(
         help=_("Number of attempts taken by the student on this problem"),
